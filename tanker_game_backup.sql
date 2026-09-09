155e8afd53cf8a2f55b61d732f4790d16485768f
wangqs <576222103@qq.com>
2026-09-09 09:59:39 +0800

    ``` docs(database): 移除mysqldump警告信息
    移除了备份文件开头的mysqldump安全警告信息，使数据库备份文件更加简洁。
    ```

--- tanker_game_backup.sql
+++ tanker_game_backup.sql
@@ -1,4 +1,4 @@
-mysqldump: [Warning] Using a password on the command line interface can be insecure.
+
 -- MySQL dump 10.13  Distrib 8.0.46, for Linux (x86_64)
 --
 -- Host: localhost    Database: tanker_game
