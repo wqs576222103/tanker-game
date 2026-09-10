a2326e2e55f7ba71ccdb84020c817b6858c73310
wangqs <576222103@qq.com>
2026-09-10 17:49:19 +0800

    ``` refactor(router): 移除移动端坦克游戏路由配置
    移除了不再使用的移动端坦克游戏页面路由配置，该页面已整合到主游戏页面中，
    不再需要独立的路由路径。
    ```

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -13,11 +13,6 @@ const routes = [
     component: () => import("../views/TankGame/index.vue"),
   },
   {
-    path: "/tank-game-mobile",
-    name: "TankGameMobile",
-    component: () => import("../views/TankGame/mobile.vue"),
-  },
-  {
     path: "/battle-arena",
     name: "BattleArena",
     component: () => import("../views/BattleArena/index.vue"),
